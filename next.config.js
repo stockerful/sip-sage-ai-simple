a11e4402e5769ac01a7985b8fb99842a52865f6a
Erik Escalona <erikescalona@MacBookPro.lan>
2026-04-03 15:36:34 -0700

    Add missing tw-animate-css + clean next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Completely ignore TypeScript errors during build
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Also ignore ESLint
-    ignoreDuringBuilds: true,
   },
 };
 
